36a39b9e702f3311bb344cb2d23c8ef1c631bb84
Thea <ruoxinzhang@student.unimelb.edu.au>
2026-05-08 22:32:25 +1000

    created the Toast notification object, which will handle temporary pop-up messages across the site.

---
+++ js/utils.js
@@ -0,0 +1,15 @@
+/*
+ * =======================================
+ * utils.js -  / Utility Functions
+ * Toast
+ * Features: Toast notifications, theme toggle, search
+ * =======================================
+ */
+
+/*
+ * Toast Notifications /
+ *
+ * Features: Display temporary notifications
+ */
+const Toast = {
+  container: null,
